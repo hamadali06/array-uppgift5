3446c8c4e0139b9eb5d3811eeaca76f449eb6f58
Emad Iddin Ali <ea224pn@student.lnu.se>
2024-06-05 19:12:41 +0200

    Add project files.

---
+++ array uppgift5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inlamning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] svar = { "Ja", "Nej", "Kanske", "Försök igen", "Absolut", "Ahmad är inte här", "Fråga senare", "Definitivt", "Troligen", "Möjligen" };
+            Random slump = new Random();
+            while (true)
+            {
+                Console.WriteLine("Ställ en fråga (eller skriv 'a' för att avsluta):");
+                string fråga = Console.ReadLine();
+                if (fråga == "a") break;
+                Console.WriteLine(svar[slump.Next(svar.Length)]);
+            }
+        }
+    }
+}
